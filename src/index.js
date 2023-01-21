665eada006a334384be926ace5d9f2a8899cf84a
badr <badrhand@gmail.com>
2023-01-21 18:38:24 +0100

    change app from node to DOM

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import "./index.css";
-// import App from "./App";
-
-// ReactDOM.render(<App />, document.getElementById("root"));
 import React from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { createRoot } from 'react-dom/client';
-const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App/>);
+
+ReactDOM.render(<App />, document.getElementById("root"));
+// import React from "react";
+// import "./index.css";
+// import App from "./App";
+// import { createRoot } from 'react-dom/client';
+// const container = document.getElementById('root');
+// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(<App/>);
